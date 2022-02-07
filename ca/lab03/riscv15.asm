98307924db44fbdbc7b428dd7431ab42b266bf4f
Lyapina Ekaterina <72204301+18kate@users.noreply.github.com>
2022-02-08 01:41:59 +0300

    Add files via upload

---
+++ ca/lab03/riscv15.asm
@@ -0,0 +1,16 @@
+#TASK 15 (x == (y + 3)) ? 0 : 1
+li a7, 5 
+ecall
+mv t0, a0  
+ecall
+mv t1, a0 
+
+addi t1, t1, 3		
+neg t1, t1		
+add t3, t0, t1		
+seqz t4, t3		
+xori t5, t4, 1
+
+li a7, 1
+mv a0, t5
+ecall
